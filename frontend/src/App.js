2d34f8c159e2e04d8669cda247ab0b0e071a10ef
KP <ngockhanh1116@gmail.com>
2022-08-23 17:16:03 +0700

    23/8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,42 @@
 import data from './data';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">NFTworld</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        {data.products.map((product) => (
-          <div className="products" key={product.slug}>
-            <div className="product">
-              <a href={`product/{product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
-              </a>
-              <div className="product-info">
-                <a href={`product/{product.slug}`}>
-                  <p>{product.name}</p>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">NFTworld</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          <h1>Featured Products</h1>
+          {data.products.map((product) => (
+            <div className="products" key={product.slug}>
+              <div className="product">
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name}></img>
                 </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </main>
-    </div>
+          ))}
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,60 @@
+import { useEffect, useReducer, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+  //   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <header>
+        <a href="/">NFTworld</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        {products.map((product) => (
+          <div className="products" key={product.slug}>
+            <div className="product">
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </Link>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
+export default HomeScreen;
