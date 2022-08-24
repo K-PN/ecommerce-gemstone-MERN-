61b839d4276d96764ca4ad48180832893dd284b8
KP <ngockhanh1116@gmail.com>
2022-08-24 21:01:52 +0700

    24/8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,34 @@ import data from './data';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">NFTworld</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>NFTword</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
-          <h1>Featured Products</h1>
-          {data.products.map((product) => (
-            <div className="products" key={product.slug}>
-              <div className="product">
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name}></img>
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            </div>
-          ))}
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <fotter>
+          <div className="text_center">All rights reserved</div>
+        </fotter>
       </div>
     </BrowserRouter>
   );
