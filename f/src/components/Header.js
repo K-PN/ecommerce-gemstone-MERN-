bbe1cb810eb1d74a8fdc139729243a2f94e24622
KP <ngockhanh1116@gmail.com>
2022-09-01 20:54:35 +0700

    1/9

--- f/src/components/Header.js
+++ f/src/components/Header.js
@@ -1,39 +1,65 @@
-import { Container, Row, Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Navbar, Nav, Image, Dropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Cart, Person, List, ListNested } from 'react-bootstrap-icons';
 import SearchBox from './SearchBox';
+import './Header.css';
 
 export default function Headers() {
   return (
     <header>
       <Container>
         <Row>
-          <LinkContainer to="/">
-            <Navbar.Brand className="col-lg-3 col-sm-4 col-md-3 col-xs-7 col-ts-12">
-              NFTword
-            </Navbar.Brand>
-          </LinkContainer>
-          <Row className="col-lg-5">
+          <Nav className="col">
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <Image
+                  src="https://opensea.io/static/images/logos/opensea.svg/"
+                  width="40"
+                />
+                NFTWorld
+              </Navbar.Brand>
+            </LinkContainer>
+          </Nav>
+          <Row className="col-6">
             <SearchBox />
           </Row>
-          <Row className="col-lg-4">
+          <Row className="w-auto col">
             <Nav className="justify-content-end">
               <Nav.Item className="nft-mini-cart">
-                <Nav.Link to="/">Cart</Nav.Link>
-                <Nav></Nav>
+                <Nav.Link to="/">
+                  <Cart size={28} color="black" />
+                </Nav.Link>
               </Nav.Item>
               <Nav.Item className="nft-account">
-                <Nav.Link to="/">Account</Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="menu-bar">
-                <Nav.Link to="/">Menu</Nav.Link>
+                <Nav.Link to="/">
+                  <Person size={33} color="black" />
+                </Nav.Link>
               </Nav.Item>
             </Nav>
           </Row>
         </Row>
       </Container>
-      <Navbar>
+      <Navbar className="header-nav-container">
         <Container>
-          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+          <Dropdown>
+            <Dropdown.Toggle variant="dark" size="lg" id="">
+              <List
+                style={{
+                  marginBottom: '5px',
+                  marginRight: '15px',
+                  color: 'white',
+                  fontSize: '1.5em',
+                }}
+              />
+              {/* <ListNested /> */}
+              All Categories
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Container>
       </Navbar>
     </header>
--- f/src/components/Product.js
+++ f/src/components/Product.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
 
 function Product(props) {
